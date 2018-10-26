0acd4c28258715b6f621eac12720fef1754562f3
Jörg Thalheim <joerg@thalheim.io>
2018-10-26 17:56:30 +0100

    go: switch to 1.11 modules

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,7 +1,7 @@
 import (
   fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/8070a6333f3fc41ef93c2b0e07f999459615cc8d.tar.gz;
-    sha256 = "0v6nycl7lzr1kdsy151j10ywhxvlb4dg82h55hpjs1dxjamms9i3";
+    url = https://github.com/NixOS/nixpkgs/archive/5f33fbbc7beef255c411fcbc3c4dea30fb260d6d.tar.gz;
+    sha256 = "04scz08pfwic1byg30zrdhw6wmqjklxcblkxh12gk6kgj6zbnqbw";
   }
 ) {
   config = {};
