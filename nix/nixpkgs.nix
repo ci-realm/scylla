bd86add0ebfa4005e7f3e1ec72507691208f3539
Michael Fellinger <michael.fellinger@xing.com>
2018-10-29 14:22:12 +0100

    Use nixpkgs with better support for go 1.11

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,7 +1,7 @@
 import (
   fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/5f33fbbc7beef255c411fcbc3c4dea30fb260d6d.tar.gz;
-    sha256 = "04scz08pfwic1byg30zrdhw6wmqjklxcblkxh12gk6kgj6zbnqbw";
+    url = https://github.com/NixOS/nixpkgs/archive/e8df5045cac40f9919e7b14d356308ace53a947e.tar.gz;
+    sha256 = "09k3azns0cwhndgklz5yhkka7a509s5nyvnfz30pvrl1gj4qqg2w";
   }
 ) {
   config = {};
