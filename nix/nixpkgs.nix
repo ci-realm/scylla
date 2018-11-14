238f47fde1905287937ed61f57d34e464d8e83e4
Michael Fellinger <michael.fellinger@xing.com>
2018-11-14 21:59:52 +0100

    fix nixpkgs version

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,11 +1,7 @@
 import (
-  # fetchTarball {
-  #   url = https://github.com/NixOS/nixpkgs/archive/e8df5045cac40f9919e7b14d356308ace53a947e.tar.gz;
-  #   sha256 = "09k3azns0cwhndgklz5yhkka7a509s5nyvnfz30pvrl1gj4qqg2w";
-  # }
   fetchTarball {
-    url = https://github.com/nixos/nixpkgs-channels/archive/c9e13806267f7fd3351d52a19cc6db5fa2985ca9.tar.gz;
-    sha256 = "0qsa3j4i2ndiw4yxla3y4i5f8r12waj34h2z84xjig4l54cx184q";
+    url = https://github.com/nixos/nixpkgs-channels/archive/a06177e65ac17ca0f043f81f6eeb5223290cbaca.tar.gz;
+    sha256 = "1fnr0sm205lj2s1pkzskva5riq8p5fiv58995p1a5gmjrfq0kw2s";
   }
 ) {
   config = {};
